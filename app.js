583c68170e2dfb3c52d56e192531ca343a9693ed
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-11-07 15:22:44 +0100

    delete sub routes

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const flash = require("connect-flash");
 const session = require("express-session");
 const mongoose = require("mongoose");
-const routes = require("./routes/index");
-const routesSub = require("./routes/sub");
 const routesRoutes = require("./routes/routes");
 const errorHandler = require("./middleware/errors");
 
@@ -16,40 +13,16 @@ const dbPass = "myRoutePass"; // edit
 //url mongodb.net
 const mongoDBUrl = `mongodb+srv://${dbUser}:${dbPass}@myroute-hytqd.mongodb.net/test2?retryWrites=true&w=majority`;
 
-//url azure.net
-// const mongoDBUrl =
-//   `mongodb://${dbUser}:c9Jt15nCpqANlJe00BNr4nyUC7F01HCp2V3BhvUCAWhY9bCvAfwcwtzoI09yZuJa8trw5aiq3Y085ZgBntjczA%3D%3D@myroutedb.documents.azure.com:10255/?ssl=true`;
+mongoose.connect(mongoDBUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-mongoose
-  .connect(mongoDBUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-//ok
-// const MongoClient = require('mongodb').MongoClient;
-// const client = new MongoClient(mongoDBUrl, { useNewUrlParser: true });
-// client.connect(err => {
-// const collection = client.db("test").collection("devices");
-// console.log("collection: ", collection)
-//   // perform actions on the collection object
-//   client.close();
-//   if(err){
-//   console.log("err: ", err)
-//   }
-// });
-
 const app = express();
-
-app.set("views", path.join(__dirname, "views"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "ejs");
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -63,11 +36,7 @@ app.use(
     cookie: {}
   })
 );
-app.use(flash());
 
-app.use("/", routes);
-app.use("/sub", routesSub);
-routesRoutes;
 app.use("/routes", routesRoutes);
 
 app.use(errorHandler.notFound);
--- controllers/PageController.js
+++
@@ -1,6 +0,0 @@
-exports.home = (req, res) => {
-  // res.sendFile(path.join(__dirname+'/../views/home.html'));
-  res.render("home.html", {
-    formMessage: req.flash("form")
-  });
-};

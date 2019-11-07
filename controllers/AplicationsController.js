07ef214d49621de1c188a33dec446db02f194a53
Mateusz Gaczorek <gaczorekmateusz@gmail.com>
2019-11-07 15:22:44 +0100

    delete sub routes

--- controllers/AplicationsController.js
+++
@@ -1,9 +0,0 @@
-exports.store = (req, res) => {
-  req.flash("form", req.body.name + " You are a true hero!");
-  res.redirect("/");
-  //   res.json({
-  //     'name': req.body.name,
-  //     'nr': req.body.nr,
-  //     'mess': req.body.mess
-  //   });
-};

3d882d015983aa884f2fe6b72527db50b268aa69
Mateusz.Gumulinski <guma1001@gmail.com>
2019-12-19 15:50:40 +0100

    photo

--- controllers/routes/createRoute.js
+++ controllers/routes/createRoute.js
@@ -1,6 +1,20 @@
 const { Routes } = require("../../models/routes");
 
 module.exports = createRoute = async (req, res) => {
+  const photos = req.body.photos;
+
+  photos.map(photo => {
+    const index = req.body.coords.findIndex(
+      point =>
+        point.latitude === photo.latitude && point.longitude === photo.longitude
+    );
+
+    req.body.coords[index] = {
+      ...req.body.coords[index],
+      image: photo.photoBase64
+    };
+  });
+
   const route = new Routes({
     name: req.body.name,
     startDate: req.body.startDate,
